d72bf4ace59ab2eace6eafde7b11eb3b7c263819
jordan <jordo-t@hotmail.com>
2025-04-30 18:48:53 +1200

    Testing CI

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 // jest.config.js
 module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-};
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    // transform JS, JSX, TS, TSX with babel-jest
+    '^.+\.(js|jsx|ts|tsx)': 'babel-jest',
+    },
+    moduleNameMapper: {
+    // mock CSS imports
+    '\\.(css|scss)': 'identity-obj-proxy',
+    // map @/ to <rootDir>/src/
+    '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    };
